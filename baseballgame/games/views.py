063c1edbec44527fc744efa1136e3bfb08ca7a6d
김범기 <dyd1308@naver.com>
2024-06-08 17:18:48 +0900

    "숫자 야구 현황판 제작중"

--- baseballgame/games/views.py
+++ baseballgame/games/views.py
@@ -1,22 +1,30 @@
 import json
 import random
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
 
 # Create your views here.
-
+@require_http_methods(["GET"])
 def home(request):
     if request.method == "GET":
         return render(request, 'home.html')
 
+@require_http_methods(["GET"])
 def soloPlay1(request):
     if request.method == "GET":
         return render(request, 'soloPlay1.html')
 
+
+@require_http_methods(["POST"])
 def soloPlay2(request):
-    if request.method == "GET":
-        return render(request, 'soloPlay2.html')
-    elif request.method == "POST":
+    # if request.method == "GET":
+    #     return render(request, 'soloPlay2.html')
+    # elif request.method == "POST":
+    if request.method == "POST":
         selected_numbers = request.POST.get('selectedNumbers')
+        if selected_numbers == "" or len(selected_numbers) != 3:
+            return redirect("soloPlay1")
         selected_numbers_int = []
         for i in range(len(selected_numbers)):
             selected_numbers_int.append(int(selected_numbers[i]))
